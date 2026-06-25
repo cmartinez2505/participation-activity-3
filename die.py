7bee66a50e15a0b23ba018700817d58c2930e1ba
Chris <chrismtz2003@gmail.com>
2026-06-25 00:18:53 -0400

    Added my header to my die.py file

---
+++ die.py
@@ -0,0 +1,9 @@
+"""
+Name: Chris Martinez
+Assignment: Participation Activity 3
+File Purpose: to make the class Die roll the virtual die 10 times with customizable number of sides
+Date: June 25, 2026
+"""
+
+
+
