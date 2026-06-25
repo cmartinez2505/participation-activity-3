8dff85c24ad7204ad475b3498419504472668cb9
Chris <chrismtz2003@gmail.com>
2026-06-25 00:34:57 -0400

    Added my roll_die() behavior

--- die.py
+++ die.py
@@ -11,5 +11,7 @@ class Die:
     def __init__(self, sides=6):
         self.sides = sides
 
-    
+    def roll_die(self):
+        random_num = random.randint(1, self.sides)
+        print(f"Rolled a {random_num}")
 
